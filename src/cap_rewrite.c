962227eafaefecc4b1363e7efccd1b17b251c9b0
xdnzxy@gmail.com <123567aaA>
2021-10-21 11:34:05 +0800

    test

--- src/cap_rewrite.c
+++ src/cap_rewrite.c
@@ -72,6 +72,7 @@ void *cap_rewrite(void* arg) {
 					struct fingerprint* newFP = (struct fingerprint*)malloc(sizeof(fingerprint));
 					assert(newFP != NULL);
 					memcpy(newFP, c->fp, sizeof(fingerprint));
+					printf("size:%lu, newFPPtr:%lu, c->fpPtr:%lu\n", sizeof(fingerprint), (uint64_t)newFP, (uint64_t)c->fp);
 					assert(existing != NULL);
                     g_hash_table_insert(existing, newFP, NULL);
                     //*************
